948717036c403d92afbd99c0123134fece344e21
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-01-21 23:04:05 -0800

    Add custom Enumerable methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,39 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    self.each_with_index { |element, idx| yield(element, idx) }
+    return self
+  end
+
+  def my_select
+    self.select { |element| yield(element) }
+  end
+
+  def my_all?
+    self.all? { |element| yield(element) }
+  end
+
+  def my_any?
+    self.any? { |element| yield(element) }
+  end
+
+  def my_none?
+    self.none? { |element| yield(element) }
+  end
+
+  def my_count
+    return self.size unless block_given?
+    self.select { |element| yield(element) }.size
+  end
+
+  def my_map
+    self.map { |element| yield(element) }
+  end
+
+  def my_inject(sum = 0)
+    return self.inject { |total, element| yield(total, element) } + sum if sum > 1
+    self.inject { |total, element| yield(total, element) }
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +42,8 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    self.each { |element| yield(element) }
+    return self
+  end
 end
